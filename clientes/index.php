f79422200107a4341637bd232a57777bce4fbb93
Juan Diaz <jcdiazqz@gmail.com>
2021-11-14 09:01:34 -0500

    Agregado Crear y Modificar Clientes

--- clientes/index.php
+++ clientes/index.php
@@ -1,125 +1,95 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Lista de Clientes</title>
-<link rel="stylesheet" type="text/css" href="hoja.css">
-
-
-</head>
-
-<body>
 <?PHP
-include("../conexion.php");
-$registros=$base->query("SELECT * FROM clientes")->fetchAll(PDO::FETCH_OBJ);
-if(isset($_POST["cr"])){
 
-$Id=$_POST["Id"];
-$Dv=$_POST["Dv"];
-$Tipo=$_POST["Tipo"];  
-$Nom=$_POST["Nom"];
-$Razon=$_POST["Razon"];
-$Depto=$_POST["Depto"];
-$Ciud=$_POST["Ciud"];
-$Dir=$_POST["Dir"];
-$Telu=$_POST["Telu"];  
-$Teld=$_POST["Teld"];
-$Mov=$_POST["Mov"];
-$Idus=$_POST["Idus"];
-$sql="INSERT INTO clientes (ID,DV,TIPO_DOC,NOMBRE, RAZON_SOCIAL,DEPARTAMENTO,CIUDAD,DIRECCION,TELEFONO1,TELEFONO2,MOVIL,ID_USUARIO)VALUES(:Id,:Dv,:Tipo,:Nom,:Razon,:Depto,:Ciud,:Dir,:Telu,:Teld,:Mov,:Idus)";
+include("../principal/arriba.php");
 
-$resultado=$base-> prepare($sql);
-$resultado-> execute(array(":Id"=>$Id,":Dv"=>$Dv,":Tipo"=>$Tipo,":Nom"=>$Nom,":Razon"=>$Razon,":Depto"=>$Depto,":Ciud"=>$Ciud,":Dir"=>$Dir,":Telu"=>$Telu,":Teld"=>$Teld,":Mov"=>$Mov,":Idus"=>$Idus));
-header("location:index.php");
+if(isset($_POST["crearcliente"])){
+    $sql="INSERT INTO clientes
+    (ID, DV, TIPO_DOC, NOMBRE, RAZON_SOCIAL, DEPARTAMENTO, CIUDAD, DIRECCION, TELEFONO1, TELEFONO2, MOVIL, ID_USUARIO)
+    VALUES(:ID, :DV, :TIPO_DOC, :NOMBRE, :RAZON_SOCIAL, :DEPARTAMENTO, :CIUDAD, :DIRECCION, :TELEFONO1, :TELEFONO2, :MOVIL, :ID_USUARIO)";
+
+    $resultado=$conexion->prepare($sql);
+    $resultado->execute(array(":ID"=>$_POST["codigo"], ":DV"=>$_POST["numero"], ":TIPO_DOC"=>$_POST["documento"], ":NOMBRE"=>$_POST["nombre"],
+     ":RAZON_SOCIAL"=>$_POST["social"], ":DEPARTAMENTO"=>$_POST["departamento"], ":CIUDAD"=>$_POST["ciudad"], ":DIRECCION"=>$_POST["direccion"],
+     ":TELEFONO1"=>$_POST["telefono1"], ":TELEFONO2"=>$_POST["telefono2"], ":MOVIL"=>$_POST["movil"], ":ID_USUARIO"=>$_POST["id_usuario"]));
+    $mensaje = "Cliente Registrado Ok";
 
 }
 
+$registros = $conexion->query("SELECT * FROM usuarios")->fetchAll(PDO::FETCH_OBJ);
+
 ?>
-
-<h1>Lista de Clientes</h1>
-<form action="<?php echo $_SERVER ['PHP_SELF']; ?>" method="post">
-  <table width="80%" border="0" align="center">
-    <tr >
-      <td class="primera_fila">ID</td>
-      <td class="primera_fila">DV</td>
-      <td class="primera_fila">TIPO_DOC</td>
-      <td class="primera_fila">NOMBRE</td>
-      <td class="primera_fila">RAZON_SOCIAL</td>
-      <td class="primera_fila">DEPARTAMENTO</td>
-      <td class="primera_fila">CIUDAD</td>
-      <td class="primera_fila">DIRECCION</td>
-      <td class="primera_fila">TELEFONO1</td>
-      <td class="primera_fila">TELEFONO2</td>
-      <td class="primera_fila">MOVIL</td>
-      <td class="primera_fila">ID_USUARIO</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>
-      <td class="sin">&nbsp;</td>      
-    </tr> 
-
-    <?php
-        foreach($registros as $clie):?>
-		
-   	<tr>
-      <td><?php echo $clie->ID?></td>
-      <td><?php echo $clie->DV?></td>
-      <td><?php echo $clie->TIPO_DOC?></td>
-      <td><?php echo $clie->NOMBRE?></td>
-      <td><?php echo $clie->RAZON_SOCIAL?></td>
-      <td><?php echo $clie->DEPARTAMENTO?></td>
-      <td><?php echo $clie->CIUDAD?></td>
-      <td><?php echo $clie->DIRECCION?></td>
-      <td><?php echo $clie->TELEFONO1?></td>
-      <td><?php echo $clie->TELEFONO2?></td>
-      <td><?php echo $clie->MOVIL?></td>
-      <td><?php echo $clie->ID_USUARIO?></td>      
- 
-     <td class="bot"><a href="borrar.php?Id=<?php echo $clie->ID?>"><input type='button' name='del' id='del' value='Borrar'></a></td>
-
-      <td class='bot'><a href="editar.php?Id=<?php echo $clie->ID?> 
-      & Dv=<?php echo $clie->DV?> 
-      & Tipo=<?php echo $clie->TIPO_DOC?> 
-      & Nom=<?php echo $clie->NOMBRE?> 
-      & Razon=<?php echo $clie->RAZON_SOCIAL?> 
-      & Depto=<?php echo $clie->DEPARTAMENTO?>
-      & Ciud=<?php echo $clie->CIUDAD?> 
-      & Dir=<?php echo $clie->DIRECCION?> 
-      & Telu=<?php echo $clie->TELEFONO1?> 
-      & Teld=<?php echo $clie->TELEFONO2?> 
-      & Mov=<?php echo $clie->MOVIL?> 
-      & Idus=<?php echo $clie->ID_USUARIO?>"><input type='button' name='up' id='up' value='Actualizar'></a></td>
-    </tr>     
-    <?php
-      endforeach;
-
-    ?>
-    
-
-	<tr>
-	  <td><input type='text' name='Id' class='centrado'></td>
-      <td><input type='text' name='Dv' class='centrado'></td>
-      <td><input type='text' name='Tipo' class='centrado'></td>
-      <td><input type='text' name='Nom' class='centrado'></td>
-      <td><input type='text' name='Razon' class='centrado'></td>
-      <td><input type='text' name='Depto' class='centrado'></td>
-      <td><input type='text' name='Ciud' class='centrado'></td>
-      <td><input type='text' name='Dir' class='centrado'></td>
-      <td><input type='phone' name='Telu' class='centrado'></td>
-      <td><input type='phone' name='Teld' class='centrado'></td>
-      <td><input type='phone' name='Mov' class='centrado'></td>
-      <td><input type='text' name='Idus' class='centrado'></td>
-      <td class='bot'><input type='submit' name='cr' id='cr' value='Insertar'></td></tr>    
-  </table>
-</form>
-<p>&nbsp;</p>
-</body>
-</html>
+    <div class="contenido_formulario">
+        <div>
+            <h2>Registo de Clientes</h2>
+        </div>
+        <div>
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+                <table class="table_formulario">
+                    <tr>
+                        <td>Código</td>
+                        <td><input type="text" name="codigo"></td>
+                    </tr>
+                    <tr>
+                        <td>Numero DV</td>
+                        <td><input type="text" name="numero"></td>
+                    </tr>
+                    <tr>
+                        <td>Tipo Documento</td>
+                        <td><input type="text" name="documento" ></td>
+                    </tr>
+                    <tr>
+                        <td>Nombres</td>
+                        <td><input type="text" name="nombre" ></td>
+                    </tr>
+                    <tr>
+                        <td>Razón Social</td>
+                        <td><input type="text" name="social" ></td>
+                    </tr>
+                    <tr>
+                        <td>Departamento</td>
+                        <td><input type="text" name="departamento" ></td>
+                    </tr>
+                    <tr>
+                        <td>Ciudad</td>
+                        <td><input type="text" name="ciudad" ></td>
+                    </tr>
+                    <tr>
+                        <td>Dirección</td>
+                        <td><input type="text" name="direccion" ></td>
+                    </tr>
+                    <tr>
+                        <td>Teléfono 1</td>
+                        <td><input type="text" name="telefono1" ></td>
+                    </tr>
+                    <tr>
+                        <td>Teléfono 2</td>
+                        <td><input type="text" name="telefono2" ></td>
+                    </tr>
+                    <tr>
+                        <td>Movil</td>
+                        <td><input type="text" name="movil" ></td>
+                    </tr>
+                    <tr>
+                        <td>ID Usuario</td>
+                        <td>
+                        <select name="id_usuario" id="id_usuario">
+                            <?PHP foreach($registros as $usuario):?>
+                                <option value="<?php echo $usuario->ID?>"><?php echo $usuario->USUARIO?></option>
+                            <?php endforeach;?>
+                        </select> 
+                        </td>
+                    </tr>
+        </div>
+        <div>
+            <tr>
+                <td class="boton_fomulario" colspan="2">
+                    <input type="submit" name="crearcliente" id="crearcliente" value="Guardar">
+                    <input type="reset" name="bot_limpiar" id="bot_limpiar" value="Limpiar">
+                </td>
+            </tr>
+            </table>
+            </form>
+        </div>
+    </div>
+    <script><?php If(isset($mensaje)) {echo "alert('".$mensaje."');";} ?></script>
+<?php include("../principal/abajo.php"); ?>
--- clientes/listar.php
+++ clientes/listar.php
@@ -1,31 +1,35 @@
 <?php include("../principal/arriba.php");
 
-$campos = $conexion->query("SHOW COLUMNS FROM inventarios.usuarios")->fetchAll(PDO::FETCH_OBJ);
+$campos = $conexion->query("SHOW COLUMNS FROM inventarios.clientes")->fetchAll(PDO::FETCH_OBJ);
 $mensaje;
 
 if (isset($_GET["ID"])) {
     $Id = $_GET["ID"];
-    $conexion->query("DELETE FROM usuarios WHERE ID='$Id'");
+    $conexion->query("DELETE FROM clientes WHERE ID='$Id'");
     $mensaje = "Elminado OK";
 } elseif (isset($_POST["ID"])) {
     $arrayField = array();
     foreach ($campos as $campo) :
-        if ($campo->Field != "ID") {
-            $arrayField[] = $campo->Field . "='" . $_POST["$campo->Field"] . "'";
+        if (!empty($_POST["$campo->Field"]) && $campo->Field != "ID") {
+            if (strpos($campo->Type,'int')){
+                $arrayField[] = $campo->Field . "=" . $_POST["$campo->Field"];
+            } else {
+                $arrayField[] = $campo->Field . "='" . $_POST["$campo->Field"] . "'";
+            }
         }
     endforeach;
     $dataSet = implode(",", ($arrayField));
-    $sql = "UPDATE usuarios SET $dataSet WHERE ID={$_POST["ID"]}";
+    $sql = "UPDATE clientes SET $dataSet WHERE ID='{$_POST["ID"]}'";
     $resultado = $conexion->prepare($sql);
     $resultado->execute();
     $mensaje = "Actualizado OK";
 }
 
-$registros = $conexion->query("select * FROM usuarios")->fetchAll(PDO::FETCH_OBJ);
+$registros = $conexion->query("select * FROM clientes")->fetchAll(PDO::FETCH_OBJ);
 ?>
 <div class="contenedor_listado">
     <div class=titulo_pagina>
-        <h1>Lista de Usuarios</h1>
+        <h1>Lista de Clientes</h1>
     </div>
     <div class=registros>
         <table class="tabla_lista">
@@ -46,13 +50,13 @@ $registros = $conexion->query("select * FROM usuarios")->fetchAll(PDO::FETCH_OBJ
                                     <?php if ($llave == "ID") { ?>
                                         <?php echo $dato;
                                         $valor = $dato ?>
-                                    <?php } elseif ($llave == "ID_ROL") { ?>
+                                    <?php } elseif ($llave == "ID_USUARIO") { ?>
                                         <select name="<?php echo $llave; ?>" id="<?php echo $llave; ?>">
-                                            <?PHP $roles = $conexion->query("select * FROM roles")->fetchAll(PDO::FETCH_OBJ);
-                                            foreach ($roles as $rol) : ?>
-                                                <option value="<?php echo $rol->ID ?>" <?php if ($dato == $rol->ID) {
+                                            <?PHP $usuarios = $conexion->query("select * FROM usuarios")->fetchAll(PDO::FETCH_OBJ);
+                                            foreach ($usuarios as $usuario) : ?>
+                                                <option value="<?php echo $usuario->ID ?>" <?php if ($dato == $usuario->ID) {
                                                                                             echo "selected";
-                                                                                        } ?>><?php echo $rol->ROL ?></option>
+                                                                                        } ?>><?php echo $usuario->USUARIO ?></option>
                                             <?php endforeach; ?>
                                         </select>
                                     <?php } else { ?>
@@ -62,13 +66,13 @@ $registros = $conexion->query("select * FROM usuarios")->fetchAll(PDO::FETCH_OBJ
                             <?php endforeach; ?>
                             <td>
                                 <input type="hidden" name="ID" id="ID" value="<?php echo $valor; ?>">
-                                <input type="submit" name="actualizarusuario" id="actualizarusuario" value="Actualizar">
+                                <input type="submit" name="actualizarclientes" id="actualizarclientes" value="Actualizar">
                     </form>
                     </td>
                     <td>
                         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
                             <input type="hidden" name="ID" id="ID" value="<?php echo $valor; ?>">
-                            <input type="submit" name="eliminarusuario" id="eliminarusuario" value="Borrar">
+                            <input type="submit" name="eliminarclientes" id="eliminarclientes" value="Borrar">
                         </form>
                     </td>
                     </tr>
