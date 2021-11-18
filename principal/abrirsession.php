353dabbcac46a6f5796276a8e77e801306674bee
Juan Diaz <jcdiazqz@gmail.com>
2021-11-18 08:40:36 -0500

    Agregado cambio de contraseña e inicio sesión

---
+++ principal/abrirsession.php
@@ -0,0 +1,20 @@
+<?PHP include("../principal/arriba.php"); ?>
+    <div>
+        <div>
+            <h3>Inicie Sessión, Ingrese Usuario y Contraseña</h3>
+        </div>
+        <div class="caja_credencial_base">
+            <div class="caja_credencial">
+            <div class="caja_credencial_arriba"></div>
+            <div>
+                <form action="" method="post">
+                    <div class="caja_credencial_input">Usuario<input type="text" name="usuario"></div>
+                    <div class="caja_credencial_input">Contraseña<input type="text" name="contraseña"></div>
+                    <div class="caja_credencial_submit"><input type="submit" Value="Enviar">
+                    <input type="reset" Value="Limpiar"></div>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+<?php include("../principal/abajo.php"); ?>
---
+++ usuarios/cambiarclave.php
@@ -0,0 +1,20 @@
+<?PHP include("../principal/arriba.php"); ?>
+    <div>
+        <div>
+            <h3>Cambio de Contraseña</h3>
+        </div>
+        <div class="caja_credencial_base">
+            <div class="caja_credencial">
+            <div class="caja_credencial_arriba"></div>
+            <div>
+                <form action="" method="post">
+                    <div class="caja_credencial_input">Nueva Contraseña<input type="text" name="usuario"></div>
+                    <div class="caja_credencial_input">Confirmar Contraseña<input type="text" name="contraseña"></div>
+                    <div class="caja_credencial_submit"><input type="reset" Value="Cancelar">
+                    <input type="submit" Value="Aceptar"></div>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+<?php include("../principal/abajo.php"); ?>
